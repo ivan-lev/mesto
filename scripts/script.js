c2f5e41bcd5d5a5ed5d404e412ad1f8a1c6d7910
Ivan Lev <navilev@yandex.ru>
2023-05-27 20:34:54 +0500

    style: добавить лоска в файле скрипта

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-// =============== КОД ДЛЯ ИЗМЕНЕНИЯ ИНФОРМАЦИИ ЮЗЕРА ===============
+// =============== Изменение информации юзера ===============
 
 const openPopupButton = document.querySelector('.profile__edit-button');
 const closePopupButton = document.querySelector('.popup__close-button');
@@ -10,26 +10,26 @@ const userDescription = document.querySelector('.profile__description');
 const inputFieldName = document.querySelector('.popup__input-field_type_name');
 const inputFieldDescription = document.querySelector('.popup__input-field_type_description');
 
-/* Открыть или скрыть всплывающее окно - общая функция */
+// Открыть или скрыть всплывающее окно - общая функция
 function togglePopupWindow(popup) {
   popup.classList.toggle('popup_opened');
 }
 
-/* Открыть окно редактирования юзера */
+// Открыть окно редактирования юзера
 openPopupButton.addEventListener('click', function () {
   togglePopupWindow(popupWindow);
 });
 
-/* Закрыть окно редактирования юзера */
+// Закрыть окно редактирования юзера
 closePopupButton.addEventListener('click', function () {
   togglePopupWindow(popupWindow);
 });
 
-/* Отобразить в попа-апе данные юзера */
+// Отобразить в попа-апе данные юзера
 inputFieldName.value = userName.textContent;
 inputFieldDescription.value = userDescription.textContent;
 
-/* Сохранить на странице новые данные юзера */
+// Сохранить на странице новые данные юзера
 popupForm.addEventListener('submit', function (event) {
   event.preventDefault();
   userName.textContent = inputFieldName.value;
@@ -37,7 +37,67 @@ popupForm.addEventListener('submit', function (event) {
   togglePopupWindow(popupWindow);
 });
 
-// =============== КОД ДЛЯ ВЗАИМОДЕЙСТВИЯ С ФОТОКАРТОЧКАМИ ===============
+// =============== Код для взаимодействия с фотокарточками ===============
+
+const cardTemplate = document.querySelector('#card-template');
+const cardTemplateContent = cardTemplate.content;
+const cardTemplateElement = cardTemplateContent.querySelector('.elements__card');
+const cardsSection = document.querySelector('.elements');
+
+// Создать фотокарточку, удалить, поставить лайк
+function createCard(card) {
+  const newCard = cardTemplateElement.cloneNode(true);
+  /** @type {HTMLImageElement} */
+  const newCardImage = newCard.querySelector('.elements__photo');
+  newCardImage.src = card.link;
+  newCardImage.alt = card.name;
+  const newCardName = newCard.querySelector('.elements__photo-name');
+  newCardName.textContent = card.name;
+
+  const deleteButton = newCard.querySelector('.elements__del-button');
+  deleteButton.addEventListener('click', function () {
+    deleteButton.closest('.elements__card').remove();
+  });
+
+  const likeButton = newCard.querySelector('.elements__like-button');
+  likeButton.addEventListener('click', function () {
+    likeButton.classList.toggle('elements__like-button_active');
+  });
+
+  return newCard;
+}
+
+// =============== Взаимодействие с окном добавления фотокарточки ===============
+
+const openPhotoAddButton = document.querySelector('#add-photo-button');
+const closePhotoAddButton = document.querySelector('#close-photo-add-button');
+const popupPhotoWindow = document.querySelector('#photo-popup-window');
+const popupPhotoForm = document.querySelector('#popup__photo-form');
+
+/* Открыть окно добавления фото */
+openPhotoAddButton.addEventListener('click', function () {
+  togglePopupWindow(popupPhotoWindow);
+});
+
+/* Закрыть окно добавления фото */
+closePhotoAddButton.addEventListener('click', function () {
+  togglePopupWindow(popupPhotoWindow);
+});
+
+/* Сохранить на странице новую карточку */
+popupPhotoForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const newCardData = { name: '', link: '' };
+  newCardData.name = document.querySelector('.popup__input-field_type_photo-name').value;
+  newCardData.link = document.querySelector('.popup__input-field_type_photo-url').value;
+  const newItem = createCard(newCardData);
+  cardsSection.prepend(newItem);
+  togglePopupWindow(popupPhotoWindow);
+  document.querySelector('.popup__input-field_type_photo-name').value = '';
+  document.querySelector('.popup__input-field_type_photo-url').value = '';
+});
+
+// =============== Заполнить страницу дефолтными ккарточками ===============
 
 const initialCards = [
   {
@@ -66,70 +126,8 @@ const initialCards = [
   }
 ];
 
-const cardTemplate = document.querySelector('#card-template');
-const cardTemplateContent = cardTemplate.content;
-const cardTemplateElement = cardTemplateContent.querySelector('.elements__card');
-const cardsSection = document.querySelector('.elements');
-//
-
-/* Создать фотокарточку, удалить, поставить лайк */
-function createCard(card) {
-  const newCard = cardTemplateElement.cloneNode(true);
-  /** @type {HTMLImageElement} */
-  const newCardImage = newCard.querySelector('.elements__photo');
-  newCardImage.src = card.link;
-  newCardImage.alt = card.name;
-  const newCardName = newCard.querySelector('.elements__photo-name');
-  newCardName.textContent = card.name;
-
-  const deleteButton = newCard.querySelector('.elements__del-button');
-  deleteButton.addEventListener('click', function () {
-    deleteButton.closest('.elements__card').remove();
-  });
-
-  const likeButton = newCard.querySelector('.elements__like-button');
-  likeButton.addEventListener('click', function () {
-    likeButton.classList.toggle('elements__like-button_active');
-  });
-
-  return newCard;
-}
-
 /* Создать первые 6 фотокарточек */
 initialCards.forEach(function (item) {
   const newItem = createCard(item);
   cardsSection.prepend(newItem);
 });
-
-// =============== КОД ДЛЯ ВЗАИМОДЕЙСТВИЯ С ОКНОМ ДОБАВЛЕНИЯ ФОТОКАРТОЧКИ ===============
-
-const addPhotoButton = document.querySelector('#add-photo-button');
-const closePhotoButton = document.querySelector('#close-photo-add-button');
-
-const popupPhotoWindow = document.querySelector('#photo-popup-window');
-const popupPhotoForm = document.querySelector('#popup__photo-form');
-
-/* Открыть окно добавления фото */
-addPhotoButton.addEventListener('click', function () {
-  togglePopupWindow(popupPhotoWindow);
-});
-
-/* Закрыть окно добавления фото */
-closePhotoButton.addEventListener('click', function () {
-  togglePopupWindow(popupPhotoWindow);
-});
-
-/* Сохранить на странице новые данные юзера */
-const inputPhotoName = document.querySelector('.popup__input-field_type_name');
-const inputPhotoUrl = document.querySelector('.popup__input-field_type_photo-url');
-popupPhotoForm.addEventListener('submit', function (event) {
-  event.preventDefault();
-  const newCardData = { name: '', link: '' };
-  newCardData.name = document.querySelector('.popup__input-field_type_photo-name').value;
-  newCardData.link = document.querySelector('.popup__input-field_type_photo-url').value;
-  const newItem = createCard(newCardData);
-  cardsSection.prepend(newItem);
-  togglePopupWindow(popupPhotoWindow);
-});
-
-/* Добавить новую фотокарточку */
