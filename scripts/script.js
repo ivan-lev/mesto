2b4173217cffe0c679260fe5e1e65ef217c60f71
Ivan Lev <navilev@yandex.ru>
2023-05-27 15:07:14 +0500

    feature: добавить функцию создания карточки

--- scripts/script.js
+++ scripts/script.js
@@ -34,3 +34,53 @@ popupForm.addEventListener('submit', function (event) {
   userDescription.textContent = inputFieldDescription.value;
   togglePopupWindow(popupWindow);
 });
+
+const initialCards = [
+  {
+    name: 'Двуглавая сопка',
+    link: './images/photo-dvuglavaya-sopka.jpg'
+  },
+  {
+    name: 'Каменная река',
+    link: './images/photo-kamennaya-reka.jpg'
+  },
+  {
+    name: 'Круглица',
+    link: './images/photo-kruglitsa.jpg'
+  },
+  {
+    name: 'Откликной гребень',
+    link: './images/photo-otkliknoy-greben.jpg'
+  },
+  {
+    name: 'Вид с Круглицы',
+    link: './images/photo-vid-s-kruglitsi.jpg'
+  },
+  {
+    name: 'Вид с метеостанции',
+    link: './images/photo-vid-s-meteostansii.jpg'
+  }
+];
+
+const cardTemplate = document.querySelector('#card-template');
+const cardTemplateContent = cardTemplate.content;
+const cardTemplateElement = cardTemplateContent.querySelector('.elements__card');
+const cardsSection = document.querySelector('.elements');
+
+/* Создать фотокарточку из картинки и текста */
+function createCard(card) {
+  const newCard = cardTemplateElement.cloneNode(true);
+  /** @type {HTMLImageElement} */
+  const newCardImage = newCard.querySelector('.elements__photo');
+  newCardImage.src = card.link;
+  newCardImage.alt = card.name;
+  const newCardName = newCard.querySelector('.elements__photo-name');
+  newCardName.textContent = card.name;
+  return newCard;
+}
+
+/* Создать первые 6 фотокарточек */
+initialCards.forEach(function (item) {
+  const newItem = createCard(item);
+  cardsSection.prepend(newItem);
+});
