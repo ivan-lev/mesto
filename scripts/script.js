60fd2dc6b94a3a181e9d085343ab926e84f0c2e2
Ivan Lev <navilev@yandex.ru>
2023-05-27 12:48:23 +0500

    style: добавить комментарии к коду javascript

--- scripts/script.js
+++ scripts/script.js
@@ -8,21 +8,26 @@ const userDescription = document.querySelector('.profile__description');
 const inputFieldName = document.querySelector('.popup__input-field_type_name');
 const inputFieldDescription = document.querySelector('.popup__input-field_type_description');
 
+/* Открыть или скрыть всплывающее окно */
 function togglePopupWindow(popup) {
   popup.classList.toggle('popup_opened');
 }
 
+/* Открыть окно редактирования юзера */
 openPopupButton.addEventListener('click', function () {
   togglePopupWindow(popupWindow);
 });
 
+/* Закрыть окно редактирования юзера */
 closePopupButton.addEventListener('click', function () {
   togglePopupWindow(popupWindow);
 });
 
+/* Отобразить в попа-апе данные юзера */
 inputFieldName.value = userName.textContent;
 inputFieldDescription.value = userDescription.textContent;
 
+/* Сохранить на странице новые данные юзера */
 popupForm.addEventListener('submit', function (event) {
   event.preventDefault();
   userName.textContent = inputFieldName.value;
