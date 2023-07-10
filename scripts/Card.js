882c9c133b52c8682cf42c37179dc33a451a32f3
ivanlev <fanatos@mail.ru>
2023-07-11 00:20:30 +0500

    style: рефакторинг и добавить alt картинкам

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,35 +1,15 @@
-import { showPhotoWindow, cardsSection, cardTemplateElement } from './index.js';
+import { showPhotoWindow, cardsSection } from './index.js';
 
 export class Card {
-  constructor(data) {
+  constructor(data, template) {
     this._imageLink = data.imageLink;
     this._imageTitle = data.imageTitle;
+    this._template = template;
   }
 
   // находим шаблон карточки и возвращаем его
   _getTemplate() {
-    return cardTemplateElement.cloneNode(true);
-  }
-
-  generateCard() {
-    this._element = this._getTemplate();
-    this._setEventListeners();
-
-    this._element.querySelector('.cards__card-image').src = this._imageLink;
-    this._element.querySelector('.cards__card-title').textContent = this._imageTitle;
-
-    //находим в генерируемой карточке кнопку лайка и вешаем слушатель
-    this._likeButton = this._element.querySelector('.cards__like-button');
-    this._likeButton.addEventListener('click', () => {
-      this._likeButton.classList.toggle('cards__like-button_active');
-    });
-
-    this._deleteButton = this._element.querySelector('.cards__del-button');
-    this._deleteButton.addEventListener('click', () => {
-      cardsSection.removeChild(this._element);
-    });
-
-    return this._element;
+    return this._template.cloneNode(true);
   }
 
   _setEventListeners() {
@@ -37,5 +17,28 @@ export class Card {
     this._element.querySelector('.cards__card-image').addEventListener('click', () => {
       showPhotoWindow({ link: this._imageLink, name: this._imageTitle });
     });
+  }
+
+  generateCard() {
+    this._element = this._getTemplate();
+    this._setEventListeners();
+
+    this._element.querySelector('.cards__card-image').src = this._imageLink;
+    this._element.querySelector('.cards__card-image').alt = this._imageTitle;
+    this._element.querySelector('.cards__card-title').textContent = this._imageTitle;
+
+    //находим в генерируемой карточке кнопку лайка и создаем слушатель
+    this._likeButton = this._element.querySelector('.cards__like-button');
+    this._likeButton.addEventListener('click', () => {
+      this._likeButton.classList.toggle('cards__like-button_active');
+    });
+
+    //находим в генерируемой карточке кнопку удалния и создаем слушатель
+    this._deleteButton = this._element.querySelector('.cards__del-button');
+    this._deleteButton.addEventListener('click', () => {
+      cardsSection.removeChild(this._element);
+    });
+
+    return this._element;
   }
 }
